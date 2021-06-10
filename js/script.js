1142936060806a90e3acaf4effeb241e9748bd9e
pablogcdev <pablogc.dev@gmail.com>
2021-06-10 18:37:54 +0200

    solved some visual bugs in different resolutions

--- js/script.js
+++ js/script.js
@@ -4,9 +4,7 @@ const aside = document.querySelector(".side-section");
 const allSection = document.querySelectorAll(".section");
 const totalSection = allSection.length;
 
-navToggleeBtn.addEventListener("click", () =>{
-    NavToggler();
-})
+navToggleeBtn.addEventListener("click", NavToggler);
 
 function NavToggler(){
     aside.classList.toggle("open");
@@ -15,4 +13,20 @@ function NavToggler(){
     for(let i=0; i<totalSection; i++){
         allSection[i].classList.toggle("open");
     }
+}
+
+const nav = document.querySelector(".nav");
+const navList = nav.querySelectorAll("li");
+const totalNavList = navList.length;
+
+for(let i = 0; i < totalNavList; i++){
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function(){
+        if(window.innerWidth <1200){
+            NavToggler();
+        }
+    })
+}
+if(window.innerWidth <1200){
+    NavToggler();
 }
